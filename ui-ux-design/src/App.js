9436f87ce42d521a1214d4f5d901b5381135e44c
ritagujar <ritagujar14@gmail.com>
2022-03-30 11:59:15 +0530

    File & Folder Structure Setup

--- ui-ux-design/src/App.js
+++ ui-ux-design/src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers/index";
+import { Brand, CTA, Navbar } from "./components/index";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
