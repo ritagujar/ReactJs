c4a1a6751e89429c61c10f25693602044f8138b3
ritagujar <ritagujar14@gmail.com>
2024-02-18 15:33:20 +0530

    Completed the Footer and made few style modification for header

--- meme-generator/src/components/Footer.js
+++ meme-generator/src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <small>Not just code, but memes with ❤️</small>
+    </div>
+  );
 };
 
 export default Footer;
